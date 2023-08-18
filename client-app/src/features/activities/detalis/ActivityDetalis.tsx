5d2edd14cae59de532c9acff6e68bb071911a573
Darwish98 <darwishmohammed60@gmail.com>
2023-08-18 09:23:29 +0200

    styling UI

--- client-app/src/features/activities/detalis/ActivityDetalis.tsx
+++ client-app/src/features/activities/detalis/ActivityDetalis.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
-import { Button, Card, Image } from "semantic-ui-react";
+import { useEffect } from "react";
+import { Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { observer } from "mobx-react-lite";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import ActivityDetaledHeader from "./ActivityDetaledHeader";
+import ActivityDetaledInfo from "./ActivityDetaledInfo";
+import ActivityDetaledChat from "./ActivityDetaledChat";
+import ActivityDetaledSidebar from "./ActivityDetaledSidebar";
 
 export default observer(function ActivityDetalis() {
   const { activityStore } = useStore();
@@ -22,33 +25,15 @@ export default observer(function ActivityDetalis() {
   if (loadingInitial || !activity) return <LoadingComponent />;
 
   return (
-    <Card fluid>
-      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
-      <Card.Content>
-        <Card.Header>{activity.title}</Card.Header>
-        <Card.Meta>
-          <span>{activity.date}</span>
-        </Card.Meta>
-        <Card.Description>{activity.description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button.Group widths="2">
-          <Button
-            as={Link}
-            to={`/manage/${activity.id}`}
-            basic
-            color="blue"
-            content="ُEdit"
-          />
-          <Button
-            as={Link}
-            to={"/activities"}
-            basic
-            color="grey"
-            content="Cancel"
-          />
-        </Button.Group>
-      </Card.Content>
-    </Card>
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetaledHeader activity={activity} />
+        <ActivityDetaledInfo activity={activity}/>
+        <ActivityDetaledChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetaledSidebar />
+      </Grid.Column>
+    </Grid>
   );
 });
