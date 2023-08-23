d4aaad1c7a56950a2a996dee865e1c8f918c7c82
Darwish98 <darwishmohammed60@gmail.com>
2023-08-23 15:55:54 +0200

    Handling API Error Responses

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -6,19 +6,28 @@ using Domain;
 using MediatR;
 using Persistence;
 using AutoMapper;
-
+using FluentValidation;
+using Application.Core;
 
 namespace Application.Activities
 {
     public class Edit
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>> 
         {
             public Activity Activity { get; set; }
 
         }
 
-        public class Handler : IRequestHandler<Command>
+         public class CommandValidator : AbstractValidator<Command> 
+        {
+            public CommandValidator() 
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command,Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -28,12 +37,19 @@ namespace Application.Activities
                 _mapper = mapper;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id); //change to FindAsync when using SQL database 
-                _mapper.Map(request.Activity, activity); //map the request activity to the activity we found in the database
-                await _context.SaveChangesAsync(); 
-                return Unit.Value;
+                var activity = await _context.Activities.FindAsync(request.Activity.Id); 
+
+                if(activity == null) return null;
+                
+                _mapper.Map(request.Activity, activity); 
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if(!result) return Result<Unit>.Failure("Failed to update activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
