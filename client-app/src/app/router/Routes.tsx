364167fd8a141fc45fdf670184ddf8929d646ee0
Darwish98 <darwishmohammed60@gmail.com>
2023-08-28 16:05:29 +0200

    Error handling

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,8 +1,10 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetalis from "../../features/activities/detalis/ActivityDetalis";
+import TestErrors from "../../features/Errors/TestError";
+import NotFound from "../../features/Errors/NotFound";
 
 export const routes: RouteObject[] = [
   {
@@ -13,6 +15,9 @@ export const routes: RouteObject[] = [
       { path: "activities/:id", element: <ActivityDetalis /> },
       { path: "createActivity", element: <ActivityForm key="create" /> },
       { path: "manage/:id", element: <ActivityForm key="manage" /> },
+      { path: "errors", element: <TestErrors /> },
+      { path: "not-found", element: <NotFound /> },
+      { path: "*", element: <Navigate replace to="/not-found" /> },
     ],
   },
 ];
